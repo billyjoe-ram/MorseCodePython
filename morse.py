e880bdb74c2fd1b156bd0833efdb33f3a297975f
Billy Joe <billyjoersantos@outlook.com>
2022-08-02 20:27:27 -0300

    feature(morse-class): Building main class
    Adding the main class that will translate values in the project

---
+++ morse.py
@@ -0,0 +1,27 @@
+class Morse:
+    morse_dict = {}
+
+    def __init__(self):
+        self.build_morse_dict()
+
+    def build_morse_dict(self):
+        self.morse_dict = {
+            'A': '._', 'B': '_...', 'C': '_._.', 'D': '_..',
+            'E': '.', 'F': '.._.', 'G': '__.', 'H': '....',
+            'I': '..', 'J': '.___', 'K': '_._', 'L': '._..',
+            'M': '__', 'N': '_.', 'O': '___', 'P': '.__.',
+            'Q': '__._', 'R': '._.', 'S': '...', 'T': '_',
+            'U': '.._', 'V': '..._', 'W': '.__', 'X': '_.._',
+            'Y': '_.__', 'Z': '__..', '1': '.____', '2': '..___',
+            '3': '...__', '4': '...._', '5': '.....', '6': '_....',
+            '7': '__...', '8': '___..', '9': '___.', '0': '_____'
+        }
+
+    def get_morse_code(self, letter: str) -> str:
+        return self.morse_dict[letter]
+
+    def get_morse_letter(self, code: str) -> str:
+        keys = list(self.morse_dict.keys())
+        values = list(self.morse_dict.values())
+
+        return keys[values.index(code)]
